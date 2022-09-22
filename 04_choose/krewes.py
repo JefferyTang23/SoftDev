0acf2da99b001cdfd2735f146b2f6a3a6bc46bea
JefferyTang23 <jtang30@stuy.edu>
2022-09-22 13:52:14 -0400

    yeahhhh

---
+++ 04_choose/krewes.py
@@ -0,0 +1,22 @@
+'''
+Jeffery Tang
+SoftDev
+K<04> -- <Dictionaries + RNG>
+<2022>-<09>-<2>
+time spent: <>
+
+DISCO:
+
+QCC:
+
+OPS SUMMARY:
+
+'''
+
+import random as random
+
+krewes = {2:"devos1", 7:"devos2", 8:"devos3"}
+
+list1 = [2, 7, 8]
+
+print(krewes[list1[random.randint(0,2)]])
